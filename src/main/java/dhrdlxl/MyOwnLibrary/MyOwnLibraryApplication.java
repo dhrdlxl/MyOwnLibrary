10523bc828116c4fbef18236067fe113dd91709a
SeungHyunYoo <dhrdlxl@naver.com>
2020-11-22 21:41:32 +0900

    Initial commit

---
+++ src/main/java/dhrdlxl/MyOwnLibrary/MyOwnLibraryApplication.java
@@ -0,0 +1,13 @@
+package dhrdlxl.MyOwnLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOwnLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOwnLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dhrdlxl/MyOwnLibrary/MyOwnLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package dhrdlxl.MyOwnLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOwnLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
